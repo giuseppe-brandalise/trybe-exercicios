1e4d7f09474926c8bb06286983d5e59344e45786
Giuseppe <giuseppebrandalise@gmail.com>
2022-08-25 15:50:18 -0300

    Realização da prática 1

--- fundamentos/bloco-5-js-DOM-eventos-e-web-storage/dia-1-js-DOM-e-seletores/conteudo-parte-II/script.js
+++ fundamentos/bloco-5-js-DOM-eventos-e-web-storage/dia-1-js-DOM-e-seletores/conteudo-parte-II/script.js
@@ -1,2 +1,11 @@
 const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
+
+const title = document.getElementById("page-title");
+title.innerText = "Os Miseráveis";
+
+const secondParagraph = document.getElementById("second-paragraph");
+secondParagraph.innerText = "Eu não sou criativo";
+
+const subtitle = document.getElementById("subtitle");
+subtitle.innerText = "Ó vc aí!"
