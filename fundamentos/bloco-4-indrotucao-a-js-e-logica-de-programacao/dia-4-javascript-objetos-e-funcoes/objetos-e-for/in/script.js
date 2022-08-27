6704d2460d597e725d6dc0f2fe37c22032da1628
Giuseppe <giuseppebrandalise@gmail.com>
2022-08-27 14:52:44 -0300

    Realização da primeira parte dos exercicios de objetos e for/in

--- fundamentos/bloco-4-indrotucao-a-js-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-e-for/in/script.js
+++ fundamentos/bloco-4-indrotucao-a-js-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-e-for/in/script.js
@@ -1,7 +1,30 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
-console.log('Bem-vinda, ' +info.personagem)
+// console.log('Bem-vinda, ' +info.personagem);
+
+// info['recorrente'] = 'sim';
+
+// console.table(info);
+
+// for (let key in info) {
+  // console.log(key);
+// }
+
+// for (let value in info) {
+//   console.log(info[value]);
+// }
+
+// let info2 = {
+//   personagem: 'Tio Patinhas',
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: 'O último MacPatinhas',
+//   recorrente : 'sim',
+// };
+
+// for (let value in info) {
+//   console.log(info[value], ' e ', info2[value]);
+// }
