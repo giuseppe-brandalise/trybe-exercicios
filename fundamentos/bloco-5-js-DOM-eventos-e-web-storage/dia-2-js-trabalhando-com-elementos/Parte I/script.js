c8a47cc0e0f147536af55392741d911548ec1af5
Giuseppe <giuseppebrandalise@gmail.com>
2022-08-30 14:46:30 -0300

    Exercicio 3

--- fundamentos/bloco-5-js-DOM-eventos-e-web-storage/dia-2-js-trabalhando-com-elementos/Parte I/script.js
+++ fundamentos/bloco-5-js-DOM-eventos-e-web-storage/dia-2-js-trabalhando-com-elementos/Parte I/script.js
@@ -2,3 +2,5 @@ console.log(document.getElementById('elementoOndeVoceEsta'));
 
 const parentNode = document.getElementById('elementoOndeVoceEsta').parentNode;
 parentNode.style.color = 'red';
+
+document.getElementById('primeiroFilhoDoFilho').innerText = 'Primeiro filho do filho';
