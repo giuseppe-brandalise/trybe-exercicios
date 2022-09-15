52f712ddd15d15cba808587ab3e1f3c9275eb12c
Giuseppe <giuseppebrandalise@gmail.com>
2022-09-15 18:45:41 -0300

    Realização do exercicio 2 da parte 2

--- fundamentos/bloco-7-introducao-a-js-ES6-e-testes-unitarios/parte2.js
+++ fundamentos/bloco-7-introducao-a-js-ES6-e-testes-unitarios/parte2.js
@@ -1,11 +1,26 @@
-const factorial = (number) => {
-    let factorialNumber = number;
-    for (let index = number - 1; index > 0; index -= 1){
-        factorialNumber = factorialNumber * index;
+//EXERCICIO 1
+
+// const factorial = (number) => {
+//     let factorialNumber = number;
+//     for (let index = number - 1; index > 0; index -= 1){
+//         factorialNumber = factorialNumber * index;
+//     }
+//     return factorialNumber;
+// }
+
+// let result = factorial(4);
+
+// console.log(`${result} é o resultado do fatorial de 4`);
+
+//EXERCICIO 2
+
+const longestWord = (sentence) => {
+    let THElongest = '';
+    const words = sentence.split(' ');
+    for (word of words) {
+        (word.length > THElongest.length) ? THElongest = word : THElongest = THElongest;
     }
-    return factorialNumber;
+    return THElongest;
 }
 
-let result = factorial(4);
-
-console.log(`${result} é o resultado do fatorial de 4`);
+console.log(longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu'))
