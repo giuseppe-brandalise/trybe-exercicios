cbc8c6c45f1c6bdac2a3fbfec965ac3aea1c69ea
Giuseppe <giuseppebrandalise@gmail.com>
2022-10-07 16:02:06 -0300

    Realização da pratica 2

---
+++ frontEnd/js-e-testes-assincronos/dia-2-js-assincrono-fetch-API-e-async-await/pratica2/fetch.js
@@ -0,0 +1,41 @@
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   console.log(fetch(url));
+// }
+
+// fetchJoke();
+
+//Ao rodar esse código, você vai perceber que no console foi impresso Promise { <pending> } ao invés da piada. O motivo é que, se o fluxo assíncrono não for controlado, ele vai retornar o seu estado e não o dado requisitado.
+
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value));
+// }
+
+// fetchJoke();
+
+//A DE CIMA DA CERTA, AGORA ADICIONANDO UM ERRO (removemos o https:// do início da url):
+
+const fetch = require('node-fetch');
+
+const fetchJoke = () => {
+  const url = 'api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+}
+
+fetchJoke();
+
+// Algo deu errado :( 
+// TypeError: Only absolute URLs are supported
